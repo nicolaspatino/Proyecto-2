f991ec070170c1cca80a12a8cde2c3f85cc26351
unknown <2112712@labinfo.is.escuelaing.edu.co>
2018-10-31 15:33:45 -0500

    mejoras lambda

--- public_html/js/Funciones.js
+++ public_html/js/Funciones.js
@@ -23,20 +23,13 @@ function getVarsUrl()
                 try {
                 var numero = document.getElementById("num");
                 var moneda1 =document.getElementById("moneda1");
-                var moneda2 =document.getElementById("moneda2");
-                
-                url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol="+moneda1.value+"&to_symbol="+moneda2.value+"&interval=5min&apikey=71LHQU1SK9MWP8LI";
+                var moneda2 =document.getElementById("moneda2"); 
+                url ="https://mbsyj73bqa.execute-api.us-east-1.amazonaws.com/prod/recurso";
                 var xmlHttp = new XMLHttpRequest();
                 xmlHttp.open("GET", url, false); // false for synchronous request
                 xmlHttp.send(null);
-                var lista =xmlHttp.responseText;
-                lista=lista.split("\n");
-                var value=lista[15];
-                value=value.split(":");
-                value=value[1];
-                var bal=value.substr(2,value.length-3);
-                bal=parseFloat(bal*(parseInt(numero.value)));
-                document.getElementById("result").value = bal;
+                var lista = xmlHttp.responseText;
+                
                 }catch(err){}
             
         }
@@ -71,18 +64,15 @@ function genera_tabla() {
     tabla.setAttribute("border", "2");
 }
 function read(moneda1, moneda2) {
-    url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=" + moneda1 + "&to_symbol=" + moneda2+"&interval=5min&apikey=71LHQU1SK9MWP8LI";
+    url ="https://mbsyj73bqa.execute-api.us-east-1.amazonaws.com/prod/recurso";
     var xmlHttp = new XMLHttpRequest();
-    
     xmlHttp.open("GET", url, false); // false for synchronous request
     xmlHttp.send(null);
     var lista = xmlHttp.responseText;
-    lista = lista.split("\n");
-    var value = lista[15];
-    value = value.split(":");
-    value = value[1];
-     
-    var bal = value.substr(2, value.length - 3);
-    bal = parseFloat(bal);
-    return bal;
+    lista= JSON.parse(lista);
+    var k;
+    for (k = 0; k < lista.length; ++k){
+        console.log(lista[k]);        
+    }
+    
 }
